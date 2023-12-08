0c31d7245f65ac4b67a3e39866c20fcb494a4ab4
傅二毛 <120191115@qq.com>
2023-12-08 13:57:32 +0800

    1. 修改 pom 版本信息 2. StatisticsRunTimeTools 工具类添加重载方法，允许输入程序名称。

--- src/main/java/com/fuermao/tools/utils/StatisticsRunTimeTools.java
+++ src/main/java/com/fuermao/tools/utils/StatisticsRunTimeTools.java
@@ -8,11 +8,30 @@ import org.slf4j.LoggerFactory;
  */
 public class StatisticsRunTimeTools {
     private static final Logger logger = LoggerFactory.getLogger(StatisticsRunTimeTools.class);
-    public static void statistic(IStatisticsRunTimes statisticsRunTimes){
+
+    private static long count(IStatisticsRunTimes statisticsRunTimes){
         long start = System.currentTimeMillis();
         statisticsRunTimes.run();
         long end = System.currentTimeMillis();
-        long result = end - start;
-        logger.debug("运行了 {} 毫秒！",result);
+        return end - start;
+    }
+
+    /**
+     * 计算程序运行消耗的时间
+     * @param statisticsRunTimes IStatisticsRunTimes 需要被计时的应用
+     */
+    public static void statistic(IStatisticsRunTimes statisticsRunTimes){
+        long count = count(statisticsRunTimes);
+        logger.debug("运行了 {} 毫秒！",count);
+    }
+
+    /**
+     * 计算程序运行消耗的时间
+     * @param name String 应用名称
+     * @param statisticsRunTimes IStatisticsRunTimes 需要被计时的应用
+     */
+    public static void statistic(String name,IStatisticsRunTimes statisticsRunTimes){
+        long result = count(statisticsRunTimes);
+        logger.debug("{} 运行了 {} 毫秒！",name,result);
     }
 }
