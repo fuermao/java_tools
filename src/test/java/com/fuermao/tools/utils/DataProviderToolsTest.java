2059100e788091c7385381fe2150bf65c5636848
傅二毛 <120191115@qq.com>
2024-06-21 10:37:26 +0800

    1. 删除无用的引入

--- src/test/java/com/fuermao/tools/utils/DataProviderToolsTest.java
+++ src/test/java/com/fuermao/tools/utils/DataProviderToolsTest.java
@@ -2,7 +2,6 @@ package com.fuermao.tools.utils;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
